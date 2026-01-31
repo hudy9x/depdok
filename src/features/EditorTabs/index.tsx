4ecc24671e58dad139404a0a817f1cf10c1299e6
hudy@mac-air <hudy9x@gmail.com>
2026-01-31 11:45:56 +0700

    refactor: move the window drag effect to a custom hook + replace zoom component to a new one

--- src/features/EditorTabs/index.tsx
+++ src/features/EditorTabs/index.tsx
@@ -1,15 +1,22 @@
 import { useAtom } from 'jotai';
 import { Link } from 'react-router-dom';
 import { ChevronLeft } from 'lucide-react';
+
 import { tabsAtom } from '@/stores/TabStore';
 import { TabItem } from './TabItem';
 import { CreateTabButton } from './CreateTabButton';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { useWindowDrag } from '@/hooks/useWindowDrag';
+import './tab.css';
 
 export function EditorTabs() {
   const [tabs] = useAtom(tabsAtom);
 
+
+
   const styleBackButton = `w-8 h-[35px] p-2 cursor-pointer hover:opacity-90 hover:bg-background/100 opacity-90 border-x border-border flex-shrink-0`;
+
+  useWindowDrag('[data-radix-scroll-area-viewport]');
 
   return (
     <div className="ml-[65px] flex items-center h-[35px] w-full overflow-hidden" data-tauri-drag-region>
@@ -24,7 +31,7 @@ export function EditorTabs() {
         style={{ width: 'calc(100vw - 280px)' }}
         data-tauri-drag-region
       >
-        <div className="flex w-max space-x-0 h-full" data-tauri-drag-region>
+        <div id="tab-content-wrapper" className="flex w-max space-x-0 h-full" data-tauri-drag-region>
           {tabs.map((tab) => (
             <div key={tab.id} className="group flex-shrink-0" data-tauri-drag-region>
               <TabItem tab={tab} />

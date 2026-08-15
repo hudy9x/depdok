f32d5903f6b725d0170128e0ca3bd75aae0fcbce
hudy@mac-air <hudy9x@gmail.com>
2026-08-15 15:37:11 +0700

    refactor: remove Footer component and its integration from Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
 import { Titlebar } from '@/features/Titlebar';
-import { Footer } from './Footer';
 
 interface LayoutProps {
   children: ReactNode;
@@ -16,9 +15,6 @@ export function Layout({ children }: LayoutProps) {
       <main className="flex-1 min-h-0 relative overflow-hidden">
         {children}
       </main>
-
-      {/* Footer - fixed height interactive status bar */}
-      <Footer />
     </div>
   );
 }
